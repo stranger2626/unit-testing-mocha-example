6b4c317d77d2d7e5a9112db31d288f65b6a527fc
Matsvei_Murashka <Matsvei_Murashka@epam.com>
2022-05-03 13:52:53 +0600

    added tests for checking error messages

--- test/specs/getEvenNumbersFromArray.spec.js
+++ test/specs/getEvenNumbersFromArray.spec.js
@@ -13,4 +13,9 @@ describe(`getEvenNumbersFromArray`, function() {
     const arrayOfRandomNumbers = [2, 7, 12, 17, 1, 55, 32, 10];
     expect(validator.getEvenNumbersFromArray(arrayOfRandomNumbers)).to.be.eql([2, 12, 32, 10]);
   });
+
+  it(`should throw an error when one or more elements of the array is not a number`, function() {
+    const randomArray = [2, 7, null, 12, 20, 11];
+    expect(() => validator.getEvenNumbersFromArray(randomArray)).to.throw(`2,7,,12,20,11] is not an array of "Numbers"`);
+  });
 });
