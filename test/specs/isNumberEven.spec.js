12d3b4489b21231977e58d03fb3f6e82f3f7cbe9
Matsvei_Murashka <Matsvei_Murashka@epam.com>
2022-05-03 13:46:07 +0600

    added tests for checking error messages

--- test/specs/isNumberEven.spec.js
+++ test/specs/isNumberEven.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import {expect} from 'chai';
 import {NumbersValidator} from '../../app/numbers_validator.js';
 
@@ -17,5 +18,29 @@ describe(`isNumberEven positive scenarios`, function() {
 
   it(`should return false if number is odd`, function() {
     expect(validator.isNumberEven(5)).to.be.equal(false);
+  });
+
+  it(`should throw an error when provided with undefined`, function() {
+    expect(() => validator.isNumberEven(undefined)).to.throw(`[undefined] is not of type "Number" it is of type "undefined"`);
+  });
+
+  it(`should throw an error when provided with null`, function() {
+    expect(() => validator.isNumberEven(null)).to.throw(`[null] is not of type "Number" it is of type "object"`);
+  });
+
+  it(`should throw an error when provided with boolean`, function() {
+    expect(() => validator.isNumberEven(false)).to.throw(`[false] is not of type "Number" it is of type "boolean"`);
+  });
+
+  it(`should throw an error when provided with a string`, function() {
+    expect(() => validator.isNumberEven('2')).to.throw(`[2] is not of type "Number" it is of type "string"`);
+  });
+
+  it(`should throw an errow when provided with an array`, function() {
+    expect(() => validator.isNumberEven([2, 4])).to.throw(`[2,4] is not of type "Number" it is of type "object"`);
+  });
+
+  it(`should throw an errow when provided with an object`, function() {
+    expect(() => validator.isNumberEven({1: 1})).to.throw(`[[object Object]] is not of type "Number" it is of type "object"`);
   });
 });
